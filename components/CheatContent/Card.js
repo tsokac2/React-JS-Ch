7e63ca5f64119b63610343df13ad32911e69b973
unknown <tsokac2@gmail.com>
2022-12-24 07:53:41 +0000

    Updating

--- components/CheatContent/Card.js
+++ components/CheatContent/Card.js
@@ -7,26 +7,33 @@ const Card = (props) => {
 
       {/* Main grid item wraper */}
 
+
+      {/* box 1  */}
+
       <Box w="100%" py="2" bg="black">
-
-
-        <VStack
-          spacing={4}
-          align='stretch'
-        >
-          <Box h='100%' py={3} bg='red.400'>
+        <VStack spacing={4} align='stretch'>
+          <Box h='100%' py={0} bg='red.400'>
             <Flex minWidth='max-content' alignItems='center' bg="#fff">
 
-              <Center h={["2rem", "6rem"]} px={2} py={2} bg="gray.300" >
+              <Center h={["2rem", "2rem"]} px={2} py={2} bg="gray.300" >
                 <Heading as='h3' size='1xl' >Componets</Heading>
 
               </Center>
 
-              <Center h={["2rem", "6rem"]} w='100%'  px={2} py={2} bg='green.500'   >
+              <Center h={["2rem", "2rem"]} w='100%'  px={2} py={2} bg='green.500'   >
                 <Box h="1px" w='100%' mt="2px" bg='red.400'></Box>
               </Center>
 
+
             </Flex>
+
+            <Box h="4rem" w='100%' mt=".5rem" bg='#FFF'>
+              <p className="content-text">
+                main componet content
+              </p>
+            </Box>
+
+            
 
 
           </Box>
@@ -37,11 +44,64 @@ const Card = (props) => {
 
       </Box>
 
+      {/* box 2  */}
+      <Box w="100%" py="2" bg="black">
+        <VStack spacing={4} align='stretch'>
+          <Box h='100%' py={0} bg='red.400'>
+            <Flex minWidth='max-content' alignItems='center' bg="#fff">
+
+              <Center h={["2rem", "2rem"]} px={2} py={2} bg="gray.300" >
+                <Heading as='h3' size='1xl' >Componets</Heading>
+
+              </Center>
+
+              <Center h={["2rem", "2rem"]} w='100%'  px={2} py={2} bg='green.500'   >
+                <Box h="1px" w='100%' mt="2px" bg='red.400'></Box>
+              </Center>
 
 
-      <Box w="100%" h="10" bg="black"></Box>
-      <Box w="100%" h="10" bg="black"></Box>
-      <Box w="100%" h="10" bg="black"></Box>
+            </Flex>
+
+            <Box h="4rem" w='100%' mt=".5rem" bg='#FFF'>
+              <p className="content-text">
+                main componet content
+              </p>
+            </Box>
+
+            
+
+
+          </Box>
+
+
+
+        </VStack>
+
+      </Box>
+
+      {/* box 3  */}
+
+      <Box w="100%" py="2" bg="black">
+        <VStack spacing={4} align='stretch'>
+          <Box h='100%' py={0} bg='red.400'>
+            <Flex minWidth='max-content' alignItems='center' bg="#fff">
+
+              <Center h={["2rem", "2rem"]} px={2} py={2} bg="gray.300" >
+                <Heading as='h3' size='1xl' >Componets</Heading>
+
+              </Center>
+              <Center h={["2rem", "2rem"]} w='100%'  px={2} py={2} bg='green.500'   >
+                <Box h="1px" w='100%' mt="2px" bg='red.400'></Box>
+              </Center>
+            </Flex>
+            <Box h="4rem" w='100%' mt=".5rem" bg='#FFF'>
+              <p className="content-text">
+                main componet content
+              </p>
+            </Box>
+          </Box>
+        </VStack>
+      </Box>
     </CardWrap>
   );
 };
